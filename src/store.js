4c3a1d32ba97feb9eeefc3e8bceb2e8c4ba65892
Luiza Petrinina <petrinina95@gmail.com>
2025-09-08 18:16:00 +0300

    Change a file structure
    Divide the store into account slice and customer slice

--- src/store.js
+++ src/store.js
@@ -1,72 +1,7 @@
 import { combineReducers, createStore } from "redux";
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/curtomerSlice";
 
-const initialStateAccount = {
-    balance: 0,
-    loan: 0,
-    loanPurpose: ''
-};
-
-const initialStateCustomer = {
-    fullName: '',
-    nationalId: '',
-    createdAt: ''
-};
-
-function accountReducer(state = initialStateAccount, action) {
-
-    switch (action.type) {
-        case 'account/deposit':
-            return {
-                ...state,
-                balance: state.balance + action.payload,
-            };
-        case 'account/withdraw':
-            return {
-                ...state,
-                balance: state.balance - action.payload,
-            };
-        case 'account/requestLoan':
-            if (state.loan > 0) {
-                return state;
-            };
-            return {
-                ...state,
-                loan: action.payload.amount,
-                balance: state.balance + action.payload.amount,
-                loanPurpose: action.payload.purpose,
-            };
-        case 'account/payLoan':
-            return {
-                ...state,
-                loan: 0,
-                loanPurpose: '',
-                balance: state.balance - state.loan
-            };
-
-        default:
-            return state;
-
-    };
-};
-
-function customerReducer(state = initialStateCustomer, action) {
-
-    switch (action.type) {
-        case 'customer/createCustomer':
-            return {
-                ...state,
-                fullName: action.payload.fullName,
-                nationalId: action.payload.nationalId,
-                createdAt: action.payload.createdAt
-            };
-        case 'customer/updateName':
-            return {
-                ...state,
-                fullName: action.payload
-            };
-        default: return state;
-    }
-}
 
 const rootReducer = combineReducers({
     account: accountReducer,
@@ -75,27 +10,4 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer);
 
-function deposit(amount) {
-    return { type: 'account/deposit', payload: amount };
-}
-
-
-function withdraw(amount) {
-    return { type: 'account/withdraw', payload: amount };
-}
-function requestLoan(amount, purpose) {
-    return { type: 'account/requestLoan', payload: { amount, purpose } };
-}
-function payLoan() {
-    return { type: 'account/payLoan' };
-}
-
-
-function createCustomer(fullName, nationalId) {
-    return { type: 'customer/createCustomer', payload: { fullName, nationalId, createdAt: new Date().toISOString() } };
-}
-
-function updateName(fullName) {
-    return { type: 'customer/updateName', payload: fullName };
-}
-
+export default store;
