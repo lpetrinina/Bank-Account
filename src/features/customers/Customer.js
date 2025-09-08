47e79fb97193ca8fc97489eb3865ab9096ed029a
Luiza Petrinina <petrinina95@gmail.com>
2025-09-08 18:30:41 +0300

    Connect Redux with React

--- src/features/customers/Customer.js
+++ src/features/customers/Customer.js
@@ -1,5 +1,9 @@
+import { useSelector } from "react-redux";
+
 function Customer() {
-  return <h2>👋 Welcome, %NAME%</h2>;
+    const name = useSelector((store) => store.customer.fullName);
+
+    return <h2>👋 Welcome, {name}</h2>;
 }
 
 export default Customer;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 
 
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
-
 
