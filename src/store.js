3cacf1bb9a3d5a2550e4b0a0ec00420a44ce549a
Luiza Petrinina <petrinina95@gmail.com>
2025-09-09 12:35:57 +0300

    Add Thunk middlewate

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/curtomerSlice";
+import { thunk } from "redux-thunk";
 
 
 const rootReducer = combineReducers({
@@ -8,6 +9,7 @@ const rootReducer = combineReducers({
     customer: customerReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 
 export default store;
